688c760f4d9e1d5c0d38c75b5c9b634038f8979b
Caio Vinicius <69957398+caioviniciusmm@users.noreply.github.com>
2021-05-29 19:59:00 -0300

    desafio pronto

--- components/card/card.component.jsx
+++ components/card/card.component.jsx
@@ -1,15 +1,16 @@
 import React, {useState} from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Card, Paragraph, Button } from 'react-native-paper';
+import { StyleSheet, View, Vibration } from 'react-native';
+import { Card, Paragraph, Button, ProgressBar, Colors } from 'react-native-paper';
+import {Stopwatch} from 'react-native-stopwatch-timer';
 
 
 const styles = StyleSheet.create({
     card: {
-        backgroundColor: '#EBF5F7',
+        backgroundColor: '#F8E7E2',
         margin: 10,
         borderRadius: 20,
         flex: 1,
-        width: '50vh',
+        width: 300,
         overflow: 'hidden',
         marginBottom: 40
     },
@@ -28,29 +29,50 @@ const styles = StyleSheet.create({
         textAlign: 'center',
         fontSize: 20,
         flex: 1,
-        marginBottom: 20
+        marginBottom: 20,
+        fontWeight: 'bold'
     },
     rectangle: {
-        height: 20,
+        height: 12,
         width: '100%',
         backgroundColor: 'white',
-        borderColor: '#000',
-        borderWidth: 2,
-        borderRadius: 5
-      }     
+        borderRadius: 20,
+        marginBottom: 15
+      },     
     }
 );
+const options = {
+    container: {
+      padding: 5,
+      borderRadius: 5,
+      flex: 1,
+      alignSelf: 'center',
+      marginBottom: 8
+    },
+    text: {
+      fontSize: 13,
+      color: 'black',
+      marginLeft: 7,
+      alignItems: 'center'
+    },
+  };
 
 const CardQuote = ({task, background}) => {
     const [iconePlay, setIcon] = useState('play')
-    const [barraCor, setCorBarra] = useState('#D8848F')
+    const [isStopwatchStart, setIsStopwatchStart] = useState(false);
+    const [resetStopwatch, setResetStopwatch] = useState(false);
+    const [tempo, setTempo] = useState(0);
+    if (tempo==15){
+        Vibration.vibrate(1000)}
+
     const cliquebotao = () => {
         if (iconePlay == 'play'){
             setIcon('pause') 
-            setCorBarra('#B25963')
+            setIsStopwatchStart(!isStopwatchStart);
+            setResetStopwatch(false);
         }else {
             setIcon('play') 
-            setCorBarra('#D8848F')
+            setIsStopwatchStart(false);
         }
     }
 
@@ -63,14 +85,35 @@ const CardQuote = ({task, background}) => {
             <Card.Content style={styles.content}>
             </Card.Content>
             <Card.Actions>
-                <Button icon= {iconePlay} style={styles.botao} labelStyle={{fontSize: 100}} onPress= {cliquebotao}> 
+                <Button icon= {iconePlay} style={styles.botao} labelStyle={{fontSize: 100}} onPress= {cliquebotao} color='#8b0000'> 
                 </Button>
             </Card.Actions>
             <Paragraph style={styles.texto}>
                     {task}
             </Paragraph>
-        <View style={[styles.rectangle, { backgroundColor: barraCor}]}>
-        </View>
+            <Stopwatch
+            laps
+            msecs
+            start={isStopwatchStart}
+
+            reset={resetStopwatch}
+         
+            options={options}
+        
+            getTime={(time) => {
+              if(time > '00:00:15:00'){
+                  setIsStopwatchStart(false);
+                  setResetStopwatch(true);
+                  setIcon('play')  
+              }; 
+              let p = time.split(':');
+              let date = new Date('00','00','0000',p[0],p[1],p[2],p[3]);
+              let seg = date.getSeconds()
+              setTempo(seg)
+              } 
+            }
+            />
+            <ProgressBar progress={tempo/15} color={Colors.red800}></ProgressBar>
         </Card>
     )
 }
